3ced74474e3ca8d7e7ea11513effcf1f630eee7c
yanqian <yanqian0716@gmail.com>
2021-03-23 11:40:25 +0800

    .

--- algorithm/train.py
+++ algorithm/train.py
@@ -48,7 +48,7 @@ def train(dataLoader, model, optim, Triplet_loss, Classifier_loss, lrSche, testD
                 tLoss = tLoss / config.LOG_BATCHSIZE*anchorFts.size(0)
                 cLoss = cLoss / config.LOG_BATCHSIZE*anchorFts.size(0)
                 print(f'[epoch:%3d/' % (epoch) + 'EPOCH: %3d]' % config.TOTAL_EPOCH + '%3d:' % idx
-                      + '\t[LOSS: %.4f]' % avgLoss + '[Trip Loss: %.4f' % tLoss + '\tClass Loss: %.4f]' % cLoss)
+                      + '\t[LOSS: %.4f]' % avgLoss + '[Trip Loss: %.4f' % tLoss + '/ Class Loss: %.4f]' % cLoss)
                 avgLoss = 0
                 tLoss = 0
                 cLoss = 0
