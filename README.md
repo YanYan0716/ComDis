7360ae6bdecbf4591ac5f98dac66096cafc56ad6
yanqian <yanqian0716@gmail.com>
2021-03-24 11:43:58 +0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ComDis
+
+
+运行：python app.py
+访问： http://127.0.0.1:5000/
--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from flask import request, flash, redirect, url_for, render_template
+from flask import request, flash, redirect, url_for, render_template, Response
 import os
 from werkzeug.utils import secure_filename
 from flask_bootstrap import Bootstrap
@@ -15,20 +15,31 @@ bootstrap = Bootstrap(app)
 @app.route('/', methods=['POST', 'GET'])
 def process():
     if request.method == 'POST':
-        f = request.files.get('selectfile')
+        files = request.files.getlist('selectfile')
+        imgfile = []
+        types = ['jpg', 'png']
         basepath = os.path.join(os.path.dirname(__file__), 'static')
         if not os.path.exists(basepath):
             os.mkdir(basepath)
-        if f:
-            filename = secure_filename(f.filename)
-            types = ['jpg', 'png']
-            if filename.split('.')[-1] in types:
-                uploadpath = os.path.join(basepath, filename)
-                f.save(uploadpath)
-                flash('upload successfull ! ', 'success')
-                return render_template('base.html', imagename=filename)
-            else:
-                flash('Unknown Types!', 'danger')
+
+        if files:
+            for img in files:
+                imgname = secure_filename(img.filename)
+                if imgname.split('.')[-1] in types:
+                    imgfile.append(imgname)
+                    uploadpath = os.path.join(basepath, imgname)
+                    img.save(uploadpath)  # 将上传的图片保存
+                else:
+                    flash('Unknown Types!', 'danger')
+            flash('upload successfull', 'success')
+            # return render_template('base.html', imglist=imgfile)
+            return Response('123123')
         else:
             flash('no File Selected', 'danger')
     return render_template('base.html')
+
+
+# @app.route('/', methods=['POST', 'GET'])
+# def ComDis():
+#     print('ok ----------------')
+#     return render_template('base.html')
