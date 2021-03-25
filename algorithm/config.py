da648544aff9b6355c64b6897b492fd179a55cef
yanqian <yanqian0716@gmail.com>
2021-03-25 16:52:20 +0800

    .

--- algorithm/config.py
+++ algorithm/config.py
@@ -1,15 +1,15 @@
 import torch
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 # dataset
-ROOT_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data'
-TRAIN_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data\\train.csv'
+# ROOT_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data'
+# TRAIN_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data\\train.csv'
 # ROOT_PATH = ''  # google
 # TRAIN_PATH = '/content/cifar/train.csv'
-# ROOT_PATH = ''  # kaggle
-# TRAIN_PATH = '../input/cifar10/cifar/train.csv'
+ROOT_PATH = ''  # kaggle
+TRAIN_PATH = '../input/cifar10/cifar/train.csv'
 IMG_SIZE = 256
 CROP_SIZE = 224
-BATCH_SIZE = 2
+BATCH_SIZE = 64
 U2WEITHS_DIR = './U2net/weights/u2netp.pth'
 
 # model
