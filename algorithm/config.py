72c888b7d464c6f6e69b5e88474fe7c6f9b8e724
yanqian <yanqian0716@gmail.com>
2021-03-25 17:59:07 +0800

    .

--- algorithm/config.py
+++ algorithm/config.py
@@ -1,17 +1,17 @@
 import torch
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 # dataset
-# ROOT_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data'
-# TRAIN_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data\\train.csv'
+ROOT_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data'
+TRAIN_PATH = 'F:\\PROJECT\\ComDis\\algorithm\\data\\train.csv'
 # ROOT_PATH = ''  # google
 # TRAIN_PATH = '/content/cifar/train.csv'
-ROOT_PATH = '../input/cub-200-2011/CUB_200_2011/images'  # kaggle
+# ROOT_PATH = '../input/cub-200-2011/CUB_200_2011/images'  # kaggle
 # TRAIN_PATH = '../input/cifar10/cifar/train.csv'
-TRAIN_PATH = '../input/cub-200-2011/CUB_200_2011/train.csv'
+# TRAIN_PATH = '../input/cub-200-2011/CUB_200_2011/train.csv'
 IMG_SIZE = 256
 CROP_SIZE = 224
 BATCH_SIZE = 64
-U2WEITHS_DIR = './ComDis/U2net/weights/u2netp.pth'
+U2WEITHS_DIR = './U2net/weights/u2netp.pth'
 
 # model
 BACKBONE_ARCH = 'resnet18'
