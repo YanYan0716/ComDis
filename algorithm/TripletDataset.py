d37c2c94f4452fe3ff9323850e6c6c96b1965a04
yanqian <yanqian0716@gmail.com>
2021-03-29 11:17:41 +0800

    .

--- algorithm/TripletDataset.py
+++ algorithm/TripletDataset.py
@@ -25,7 +25,7 @@ class TripletDataset(data.Dataset):
         self.train = train
         if self.train:
             info_file = pd.read_csv(img_dir)
-            self.ImgList = info_file['file_name']
+            self.ImgList = info_file['name']
             self.LabelList = info_file['label']
         else:
             info_file = pd.read_csv(img_dir)
